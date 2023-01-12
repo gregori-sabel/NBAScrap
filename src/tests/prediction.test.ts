30b82c22c55ee023d56dba699442b1209c727fc0
Grégori Sabel <seriosabel57@gmail.com>
2023-01-12 20:49:10 -0300

    jest installed

---
+++ src/tests/prediction.test.ts
@@ -0,0 +1,12 @@
+
+import {describe, expect, test} from '@jest/globals';
+
+// import { Espn } from "../espn";
+
+// const espn = new Espn();
+
+describe('sum module', () => {
+  test('gets results from Espn', () => {
+    expect(1+1).toBe(2)
+  })
+})
