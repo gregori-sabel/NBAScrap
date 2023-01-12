455d1d2ab6dd0d270c0033ad733494725778d986
Grégori Sabel <seriosabel57@gmail.com>
2023-01-12 20:55:38 -0300

    babel solving jest import errors

--- src/tests/prediction.test.ts
+++ src/tests/prediction.test.ts
@@ -1,12 +1,29 @@
 
 import {describe, expect, test} from '@jest/globals';
+import puppeteer from 'puppeteer';
 
-// import { Espn } from "../espn";
+import { Espn } from "../espn";
 
-// const espn = new Espn();
 
 describe('sum module', () => {
-  test('gets results from Espn', () => {
+
+  const espn = new Espn();
+
+  test('1+1 = 2', () => {
+    espn.getData
     expect(1+1).toBe(2)
   })
+
+  test('gets results from Espn', async () => {
+    
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+
+    const gamesData = await espn.getData(page, { day: '10', month: '01', year: '2022'})
+    console.log(gamesData)
+    
+    await browser.close();
+  })
+
+
 })
