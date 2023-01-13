fc0153feac1db414d4ed91dacfd992f7abc91f0b
Grégori Sabel <seriosabel57@gmail.com>
2023-01-12 21:48:51 -0300

    first test and some modularizations
    Co-authored-by: Bernardo Mesko <bemesko@gmail.com>

--- src/espn.ts
+++ src/espn.ts
@@ -2,8 +2,41 @@ import { DayDataResult } from "."
 import { DateObject } from "./types/basicTypes";
 import { GameResult, ResultsDataSource } from "./types/resultTypes";
 
-export class Espn implements ResultsDataSource{
-  
+
+
+export class Espn implements ResultsDataSource {
+
+  htmlScrap() {
+    const gamesNode = document.querySelector('.gameModules')
+    const gamesNodeList = gamesNode.querySelectorAll('section .Scoreboard')
+
+
+    function scrapEachGame(game: Element) {
+      const awayTeamName = game.querySelectorAll('.ScoreCell__Truncate')[0].textContent.trim()
+      const homeTeamName = game.querySelectorAll('.ScoreCell__Truncate')[1].textContent.trim()
+    
+      const awayTeamPoints = game.querySelectorAll('.ScoreCell__Score')[0].textContent.trim()
+      const homeTeamPoints = game.querySelectorAll('.ScoreCell__Score')[1].textContent.trim()
+    
+      return {
+        home: {
+          name: homeTeamName,
+          points: homeTeamPoints
+        }, 
+        away: {
+          name: awayTeamName,
+          points: awayTeamPoints            
+        },
+      }
+    }
+
+    const gamesObject = Array.from(gamesNodeList).map(
+      (game) => scrapEachGame(game) 
+    );
+    return gamesObject
+  }
+
+
   async getData(page: any, {year, month, day}: DateObject):Promise<DayDataResult> {  
     const brFormattedDate = [day, month, year].join('/')
 
@@ -15,41 +48,16 @@ export class Espn implements ResultsDataSource{
     // Wait for the results page to load and display the results.
     await page.waitForSelector('.gameModules');
 
-    const gamesObject: GameResult[] = await page.evaluate(() => {
-      const gamesNode = document.querySelector('.gameModules')
-      const gamesNodeList = gamesNode.querySelectorAll('section .Scoreboard')
-
-      const gamesObject = Array.from(gamesNodeList).map(game => {
-
-        const awayTeamName = game.querySelectorAll('.ScoreCell__Truncate')[0].textContent.trim()
-        const homeTeamName = game.querySelectorAll('.ScoreCell__Truncate')[1].textContent.trim()
-
-        const awayTeamPoints = game.querySelectorAll('.ScoreCell__Score')[0].textContent.trim()
-        const homeTeamPoints = game.querySelectorAll('.ScoreCell__Score')[1].textContent.trim()
-
-
-        return {
-          home: {
-            name: homeTeamName,
-            points: homeTeamPoints
-          }, 
-          away: {
-            name: awayTeamName,
-            points: awayTeamPoints            
-          },
-        }
-      })
-      return gamesObject
-
-    })   
+    // Everthing inside evaluate happens inside the other browser
+    const gamesObject: GameResult[] = await page.evaluate(this.htmlScrap)
+    
 
     const dayGamesObject = {
       date: brFormattedDate,
       games: gamesObject
     }
     
-    return dayGamesObject  
-
+    return dayGamesObject
   }
 
 }
