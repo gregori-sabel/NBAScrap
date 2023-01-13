4ba745f2b8be8c163b147f1e62c111d50a1111e2
Bernardo Mesko <bemesko@gmail.com>
2023-01-12 21:54:45 -0300

    Trim whitespaces

--- src/espn.ts
+++ src/espn.ts
@@ -2,8 +2,6 @@ import { DayDataResult } from "."
 import { DateObject } from "./types/basicTypes";
 import { GameResult, ResultsDataSource } from "./types/resultTypes";
 
-
-
 export class Espn implements ResultsDataSource {
 
   htmlScrap() {
@@ -14,50 +12,47 @@ export class Espn implements ResultsDataSource {
     function scrapEachGame(game: Element) {
       const awayTeamName = game.querySelectorAll('.ScoreCell__Truncate')[0].textContent.trim()
       const homeTeamName = game.querySelectorAll('.ScoreCell__Truncate')[1].textContent.trim()
-    
+
       const awayTeamPoints = game.querySelectorAll('.ScoreCell__Score')[0].textContent.trim()
       const homeTeamPoints = game.querySelectorAll('.ScoreCell__Score')[1].textContent.trim()
-    
+
       return {
         home: {
           name: homeTeamName,
           points: homeTeamPoints
-        }, 
+        },
         away: {
           name: awayTeamName,
-          points: awayTeamPoints            
+          points: awayTeamPoints
         },
       }
     }
 
     const gamesObject = Array.from(gamesNodeList).map(
-      (game) => scrapEachGame(game) 
+      (game) => scrapEachGame(game)
     );
     return gamesObject
   }
 
-
-  async getData(page: any, {year, month, day}: DateObject):Promise<DayDataResult> {  
+  async getData(page: any, { year, month, day }: DateObject): Promise<DayDataResult> {
     const brFormattedDate = [day, month, year].join('/')
 
     console.log('espn: ', brFormattedDate)
-    console.log('https://www.espn.com.br/nba/resultados/_/data/'+ ( year+month+day ))
-      
-    await page.goto('https://www.espn.com.br/nba/resultados/_/data/'+ ( year+month+day ));
+    console.log('https://www.espn.com.br/nba/resultados/_/data/' + (year + month + day))
+
+    await page.goto('https://www.espn.com.br/nba/resultados/_/data/' + (year + month + day));
 
     // Wait for the results page to load and display the results.
     await page.waitForSelector('.gameModules');
 
     // Everthing inside evaluate happens inside the other browser
     const gamesObject: GameResult[] = await page.evaluate(this.htmlScrap)
-    
 
     const dayGamesObject = {
       date: brFormattedDate,
       games: gamesObject
     }
-    
+
     return dayGamesObject
   }
-
 }
