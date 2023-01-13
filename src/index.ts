f161a482d23c6fa1a5c281f2ed35b38d35767ae5
Bernardo Mesko <bemesko@gmail.com>
2023-01-13 10:40:33 -0300

    Remove console.logs

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import puppeteer from 'puppeteer';
 
 import { Espn } from './resultSources/espn'
 import { Oddsshark } from './predictionSources/oddsshark'
-import { getDateList, parseMatchDate } from './utils/dateHandlers'
+import { parseMatchDate } from './utils/dateHandlers'
 import { ResultsDataSource, DayDataResult } from './types/resultTypes';
 import { PredictionDataSource, DayDataPrediction } from './types/predictionTypes';
 import { savePredictionsFile, saveResultsFile } from './utils/fileSystemHandlers';
@@ -13,28 +13,18 @@ import { savePredictionsFile, saveResultsFile } from './utils/fileSystemHandlers
   const scrappedPage = await browser.newPage();
   const { matchDay, matchMonth, matchYear } = parseMatchDate(new Date('01/10/2023'));
 
-  const dateAMDArray = getDateList('12/10/2022', '12/10/2022')
-  console.log(dateAMDArray)
-
   async function getMatchResults(resultsDataSource: ResultsDataSource): Promise<DayDataResult> {
-    const matchResultData = await resultsDataSource.getData(
+    return await resultsDataSource.getData(
       scrappedPage,
       { day: matchDay, month: matchMonth, year: matchYear }
     )
-
-    console.log(matchResultData)
-
-    return matchResultData;
   }
 
   async function getMatchPredictions(predictionDataSource: PredictionDataSource): Promise<DayDataPrediction> {
-    const matchPredictionData = await predictionDataSource.getData(
+    return await predictionDataSource.getData(
       scrappedPage,
       { day: matchDay, month: matchMonth, year: matchYear }
     )
-    console.log(matchPredictionData)
-
-    return matchPredictionData
   }
 
   const espn = new Espn();
