06bd535ca1e4545639ba0aec1d5fbee50d538b83
Bernardo Mesko <bemesko@gmail.com>
2023-01-13 10:58:08 -0300

    Assure browser is always closed

--- src/index.ts
+++ src/index.ts
@@ -10,16 +10,19 @@ import { getMatchPredictions } from './getMatchPredictions';
 (async () => {
 
   const browser = await puppeteer.launch({ headless: true });
-  const scrappedPage = await browser.newPage();
-  const matchDate = toDateObject(new Date('01/10/2023'));
+  try {
+    const scrappedPage = await browser.newPage();
+    const matchDate = toDateObject(new Date('01/10/2023'));
 
-  const espn = new Espn();
-  const matchResults = await getMatchResults(espn, scrappedPage, matchDate)
-  saveResultsFile(matchResults);
+    const espn = new Espn();
+    const matchResults = await getMatchResults(espn, scrappedPage, matchDate)
+    saveResultsFile(matchResults);
 
-  const oddsshark = new Oddsshark();
-  const matchPredictions = await getMatchPredictions(oddsshark, scrappedPage, matchDate)
-  savePredictionsFile(matchPredictions)
+    const oddsshark = new Oddsshark();
+    const matchPredictions = await getMatchPredictions(oddsshark, scrappedPage, matchDate)
+    savePredictionsFile(matchPredictions)
 
-  await browser.close();
+  } finally {
+    await browser.close();
+  }
 })();
